1d16c6893ff088e7c087c15a8db15f0abe14c5da
tariq <shaikh.tariqnawaz@gmail.com>
2019-03-09 02:22:42 -0800

    changes server js

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/my-first-app'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'/dist/my-first-app/index.html'));
 });
 
 // Start the app by listening on the default Heroku port or you can add message for port in use
